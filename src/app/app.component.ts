e6b51dbfc3dffd267bf14157319290b4628ef850
Robertas <robertas.nevoinas@stud.vilniustech.lt>
2023-04-19 19:51:45 +0300

    ability to add a new todo element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,15 @@ export class AppComponent {
     new Todo('Task 2', 'This is the description for task 2')
   ];
 
+  addTodoItem(newTodoForm : NgForm) {
+    if(newTodoForm.valid){
+      const newTodo = new Todo(newTodoForm.value.todoHeader, newTodoForm.value.todoDescription);
+      this.todoList.push(newTodo);
+      newTodoForm.resetForm(); //skirtumas nuo .reset() tame, kad .resetForm() dar pakeicia subbmited i false, todel nerodys error messages po submito sekmingo
+    }
+  }
+
+
   deleteTodoItem(todo: Todo): void {
     const index = this.todoList.findIndex(t => t.id === todo.id);
     if (index !== -1) {
